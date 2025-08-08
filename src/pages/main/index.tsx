a4ff79c1593f761a9153f0995363d2f04aa81330
Jairwin <jairwin@jairwin.cn>
2025-08-08 14:45:27 +0800

    feat: unplugin-auto-import

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,11 +2,16 @@ import { AutoCenter } from '@/components'
 import { APP_NAME } from '@/constants'
 import styles from './index.module.less'
 
-export default function App() {
+export default function Page() {
+	const [count, setCount] = useState(0)
+	const handleCount = () => {
+		setCount(count + 1)
+	}
   return (
     <div className={styles.app}>
 			<AutoCenter>
-				主页面，{APP_NAME}
+			{ count }，主页面，{APP_NAME}
+			<button onClick={handleCount}>+1</button>
 			</AutoCenter>
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,56 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import AutoImport from 'unplugin-auto-import/vite';
 import path from 'path'
 
 const __dirname = path.dirname(__filename);
 const resolvePath = (dir: string) => path.join(__dirname, dir);
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+		react(),
+		AutoImport({
+			imports: ['react'],
+			dts: true, // 生成 TypeScript 声明
+			eslintrc: {
+				enabled: true, // <-- this
+			},
+		}),
+	],
 	css: {
 		preprocessorOptions: {
 			less: {
 				additionalData:
 					`@import "@/styles/global.less";@import "@/styles/mixin.less";@import "@/styles/variable.less";`,
 				javascriptEnabled: true,
+			},
+		},
+	},
+	build: {
+		commonjsOptions: {
+			transformMixedEsModules: true,
+		},
+		rollupOptions: {
+			output: {manualChunks(id: string) {
+					if (id.includes('src/root.tsx')) {
+						return 'root';
+					}
+					if (id.includes('src/components')) {
+						return 'components';
+					}
+					if (id.includes('src/utils')) {
+						return 'utils';
+					}
+					if (id.includes('src/constants')) {
+						return 'constants';
+					}
+					if (id.includes('src/typings')) {
+						return 'typings';
+					}
+					if (id.includes('src/styles')) {
+						return 'styles';
+					}
+				}
 			},
 		},
 	},
